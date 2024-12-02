e9f074f59ca5031f139d824ec14e56574ffcc141
sammythedeveloper <justsammy484@gmail.com>
2024-12-01 22:48:36 -0500

    random

--- server/controller/answerControl.js
+++ server/controller/answerControl.js
@@ -1,9 +1,9 @@
 // dbconnection
-const dbConnection = require('../db/dbConfig')
-const { v4: uuidv4 } = require('uuid');
+const dbConnection = require("../db/dbConfig");
+const { v4: uuidv4 } = require("uuid");
 const { StatusCodes } = require("http-status-codes");
 async function answerquestion(req, res) {
-  const { userid, answer, questionid } = req.body;
+  const { user_id, question_id, answer } = req.body;
   console.log(req.body);
 
   if (!answer) {
@@ -14,10 +14,12 @@ async function answerquestion(req, res) {
 
   try {
     await dbConnection.query(
-      "INSERT INTO answers(questionid,userid,answer) VALUES (?,?,?)",
-      [questionid, userid, answer]
+      "INSERT INTO answers (user_id, question_id, answer) VALUES (?, ?, ?)",
+      [user_id, question_id, answer]
     );
-    return res.status(StatusCodes.CREATED).json({ msg: "Your Answer Posted!" });    
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "Your answer has been posted!" });
   } catch (error) {
     console.log(error);
     return res
@@ -27,26 +29,30 @@ async function answerquestion(req, res) {
 }
 
 async function answerallquestions(req, res) {
-
- 
-  
+  const { question_id } = req.params;
   try {
+    // Retrieve all answers for the specified question, along with the username of the user who posted the answer
     const [answers] = await dbConnection.query(
-      "SELECT answers.answer,answers.userid,users.username,questions.questionid FROM answers JOIN users ON answers.userid = users.userid JOIN questions ON answers.questionid = questions.questionid",
-   
+      `SELECT 
+        answers.answer, 
+        answers.user_id, 
+        users.username, 
+        questions.question_id 
+      FROM answer 
+      JOIN users ON answers.user_id = users.user_id 
+      JOIN question ON answers.question_id = questions.question_id 
+      WHERE answers.question_id = ?`,
+      [question_id]
     );
     console.log(answers);
 
     if (answers.length > 0) {
-      return res.status(StatusCodes.OK).send(answers);
+      return res.status(StatusCodes.OK).json(answers);
     } else {
-      // Optionally, you might want to return a 404 if no answers are found
       return res
         .status(StatusCodes.NOT_FOUND)
-        .json({ msg: "No answers found for the specified question." });
+        .json({ msg: "No answers found for this question." });
     }
-
-
   } catch (error) {
     console.log(error);
     return res
@@ -54,8 +60,5 @@ async function answerallquestions(req, res) {
       .json({ msg: "Something went wrong, please try again later!" });
   }
 }
-
-
-
 
 module.exports = { answerallquestions, answerquestion };
--- server/controller/questionControl.js
+++ server/controller/questionControl.js
@@ -2,19 +2,17 @@
 const dbConnection = require("../db/dbConfig");
 const { v4: uuidv4 } = require('uuid');
 async function askquestion(req, res) {
+  const { question_id,user_id, question, description } = req.body;
   req.body.questionid = uuidv4();
-  const { userid,questionid,title, description, tag } = req.body;
   console.log(req.body);
 
- 
-
   //validate with condition
-  if (!title || !description) {
-    return res.status(400).json({ msg: "Require to fill all the fields" });
+  if (!question || !description) {
+    return res.status(400).json({ msg: "Please fill all required fields (question and description" });
   
   } try {
-    await dbConnection.query("INSERT INTO questions(questionid,userid,title,description, tag) VALUES (?,?,?,?,?)", [questionid,userid, title, description, tag])
-    return res.status(201).json({ msg: "question asked" });
+    await dbConnection.query("INSERT INTO questions(question_id,user_id,question,description) VALUES (?,?,?,?)", [question_id, user_id, question, description])
+    return res.status(201).json({ msg: "Question asked successfully" });
   } catch (error) {
     console.log(error.message)
     return res.status(500).json({msg:"something went wrong,try again later!"})
@@ -24,27 +22,55 @@ async function askquestion(req, res) {
 
 
 async function allquestions(req, res) {
-      try {
-    const [questionid]= await dbConnection.query(`SELECT questions.*, users.username FROM questions JOIN users ON questions.userid = users.userid`)
+  try {
+    // Query to get all questions with the username of the user who posted them
+    const [questions] = await dbConnection.query(
+      `SELECT questions.question_id, questions.user_id, questions.question, questions.description, users.username
+       FROM questions 
+       JOIN users ON questions.user_id = users.user_id`
+    );
     
-        return res.status(200).send(questionid);
-  } catch (error) {
-    console.log(error.message)
-    return res.status(500).json({msg:"something went wrong,try again later!"})
-  
-      }
-      
-}
-async function singlequestion(req, res) {
- const{id}=req.body
-    try {
-      var[userid]= await dbConnection.query(`SELECT * FROM questions WHERE id = ? `,[id] )
-      return res.json({ SingleQuestion:userid}); 
-    } catch (error) {
-      console.log(error.message)
-      return res.status(500).json({msg:"something went wrong,try again later!"})
-    
+    if (questions.length > 0) {
+      return res.status(200).json(questions);  // Return all questions with the associated username
+    } else {
+      return res.status(404).json({ msg: "No questions found" });
     }
+
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ msg: "Something went wrong, please try again later!" });
   }
+}
+
+async function singlequestion(req, res) {
+  const { question_id } = req.body;  // Destructure 'question_id' from req.body
+
+  // Validate that 'question_id' is provided and is a valid number
+  if (!question_id || isNaN(question_id)) {
+    return res.status(400).json({ msg: "Invalid question_id provided" });
+  }
+
+  try {
+    // Query to fetch a single question based on 'question_id'
+    const [question] = await dbConnection.query(
+      `SELECT question_id, user_id, question, description 
+       FROM questions 
+       WHERE question_id = ?`, [question_id]
+    );
+
+    // Check if the question was found
+    if (!question || question.length === 0) {
+      return res.status(404).json({ msg: "Question not found" });
+    }
+
+    // Return the single question
+    return res.status(200).json({ SingleQuestion: question });
+
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ msg: "Something went wrong, please try again later!" });
+  }
+}
+
   
   module.exports = { askquestion, allquestions, singlequestion }

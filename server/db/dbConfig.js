bbf242261484fc6f828aacfff5814f3f3a25c1b9
sammythedeveloper <justsammy484@gmail.com>
2025-11-20 23:18:26 -0500

    import.js

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,17 +1,13 @@
+require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  // host: "srv1102.hstgr.io",
-  // user: "u775439782_forum",
-  // password: "0911088006@Sa",
-  // database: "u775439782_evanadi"
   connectionLimit: 10,
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  host: 'localhost',
-  user: 'anotherone',
-  password: '1234',
-  database: 'anotherone'
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
-
-module.exports=dbConnection.promise()
+module.exports = dbConnection.promise();

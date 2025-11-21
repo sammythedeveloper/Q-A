9df81608fb2161ed28a17d4b537244cd6b6c2b80
sammythedeveloper <justsammy484@gmail.com>
2025-11-20 23:30:52 -0500

    prepare backend for Railway deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3500;
+const port = process.env.PORT || 3500;
 
 const cors = require("cors");
 

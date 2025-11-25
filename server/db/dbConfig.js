9e319d81a57683cee12c28ce1a38b73936a61791
sammythedeveloper <justsammy484@gmail.com>
2025-11-24 21:55:47 -0500

    service again

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -4,10 +4,10 @@ const mysql2 = require("mysql2");
 const dbConnection = mysql2.createPool({
   connectionLimit: 10,
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
+  port: Number(process.env.MYSQL_PORT), // <-- important!
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  ssl: { rejectUnauthorized: false }, // <-- needed for Railway
 });
 
-module.exports = dbConnection.promise();

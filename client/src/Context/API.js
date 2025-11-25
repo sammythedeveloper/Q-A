ccf1ef78d5ba9a8e31c18d2910bf10a8261f7e70
sammythedeveloper <justsammy484@gmail.com>
2025-11-24 21:32:16 -0500

    service

--- client/src/Context/API.js
+++ client/src/Context/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3500/api', // Replace with your backend API URL
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },

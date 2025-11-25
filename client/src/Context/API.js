e7dae08d02252c8d0659f1dbc415fa44bc43f638
sammythedeveloper <justsammy484@gmail.com>
2025-11-24 21:56:38 -0500

    context-api

--- client/src/Context/API.js
+++ client/src/Context/API.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
+
 
 export default api;
